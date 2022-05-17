2c326d502c8c5100dbef5a6df8921258ec70163e
llbyron31 <82618775+llbyron31@users.noreply.github.com>
2022-05-16 21:59:42 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Random Gif Cat</h1>
+    <forms></forms>
+  </div>
+</template>
+
+<script>
+import Forms from './components/Forms.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Forms
+  }
+}
+</script>
+
+<style>
+*{
+    margin:0;
+    padding: 0;
+}
+body{
+  background: #ADD9E6;
+  text-align: center;
+}
+</style>
